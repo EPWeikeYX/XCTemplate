d91e106a50ae89877d1bcc274673aec0a848916d
陈家宏 <197778537@qq.com>
2019-02-26 14:27:42 +0800

    模板上传

---
+++ epweike/EPWeiKe Cocoa Touch Class.xctemplate/UIViewControllerObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,65 @@
+//___FILEHEADER___
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+#pragma mark - data
+
+- (void)loadData {
+    
+}
+
+- (void)loadMoreData {
+    
+}
+
+#pragma mark - Public Method
+
+#pragma mark - Private Method
+
+#pragma mark - IBActions
+
+#pragma mark - get & set
+
+#pragma mark - UITableViewDataSource
+
+#pragma mark - UITableViewDelegate
+
+
+#pragma mark - Other Delegate
+
+#pragma mark - Get & Set
+
+#pragma mark - NSObject
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
