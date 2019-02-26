f6b5d5762e84bdc5d541a80647ab8247ff6db90b
chenjiahong <chenjiahong@epweike.com>
2019-02-26 15:29:18 +0800

    install 完善

--- install.sh
+++ install.sh
@@ -1,28 +1,34 @@
  #!/bin/bash
 #只需要在终端中输入 $ sh install.sh  即可安装完成
  
-install(){
-
 templatePath=~/Library/Developer/Xcode/Templates/
+echo ${templatePath}
+
+currentpath=$(pwd)
+
+echo ${currentpath}
 
 if [ ! -d templatePath ];
 then
-mkdir templatePath
+mkdir ${templatePath}
 else
-echo "文件夹已经存在"
+echo ${templatePath}"文件夹已经存在,跳过创建"
 fi
 
 epweikeTemplatePath=${templatePath}"epweike"
 
+epweikeLibPath=${currentpath}"/epweike"
 
+chmod -R 775 ${epweikeLibPath}
 
 if [ ! -f epweikeTemplatePath ];then
-echo "文件不存在"
+	echo ${epweikeTemplatePath}"文件夹不存在，跳过删除"
 else
-rm -f epweikeTemplatePath
+	rm -f ${epweikeTemplatePath}
 fi
 
-cp ./epweike templatePath
+cp -r ${epweikeLibPath} ${templatePath}
+
+echo "安装完成请重启Xcode"
 
 
-}
